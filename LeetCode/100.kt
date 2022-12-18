a877a8598e099d634006f0011d4e340b39cf70f4
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-12-18 21:54:50 +0900

    [LeetCode 100] Same Tree

---
+++ LeetCode/100.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if (p == null && q == null) {
+            return true
+        }
+
+        return (p?.`val` == q?.`val`) &&
+                isSameTree(p?.left, q?.left) &&
+                isSameTree(p?.right, q?.right)
+    }
+}
