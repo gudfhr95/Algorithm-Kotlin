1d1ed806b118a7fc4456eb8250e2cf4761a1a00f
Hyungrok Ham <ghldnjsdlf@gmail.com>
2023-01-26 21:36:49 +0900

    [LeetCode 217] Contains Duplicate

---
+++ LeetCode/217.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val set = mutableSetOf<Int>()
+
+        for (num in nums) {
+            if (set.contains(num)) {
+                return true
+            }
+
+            set.add(num)
+        }
+
+        return false
+    }
+}
